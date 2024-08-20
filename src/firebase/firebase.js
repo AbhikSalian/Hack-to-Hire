3a819c696c9c08461aaee029d7d42ea0c2540d2d
Abhik Salian <21e03.abhik@sjec.ac.in>
2024-08-20 16:32:50 +0530

    firebase initialized

---
+++ src/firebase/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC1DVW7IGdhaLe_mmpgPuFb2NZHRSWzGAs",
+  authDomain: "hack-to-hire.firebaseapp.com",
+  projectId: "hack-to-hire",
+  storageBucket: "hack-to-hire.appspot.com",
+  messagingSenderId: "166434181364",
+  appId: "1:166434181364:web:bf4a816a1da1cac3244ea1",
+  measurementId: "G-LWZHJP2MRJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export{app,auth};
