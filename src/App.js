a92fe9fd95739ce8c3da713a63c2c2b1cdce5049
Abhik Salian <21e03.abhik@sjec.ac.in>
2024-08-20 21:27:25 +0530

    sign up and sign in implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
-import './App.css';
-import Header from './components/Header';
+import { BrowserRouter } from "react-router-dom";
+import Login from "./components/auth/login";
+import Register from "./components/auth/register";
+import Header from "./components/header";
+import Home from "./components/home";
+import { AuthProvider } from "./contexts/authContext";
+import { useRoutes } from "react-router-dom";
+
+function AppRoutes() {
+  const routesArray = [
+    {
+      path: "*",
+      element: <Login />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/home",
+      element: <Home />,
+    },
+  ];
+
+  return useRoutes(routesArray);
+}
+
 function App() {
   return (
-    <>
-    <Header/>
-    </>
+    <AuthProvider>
+      <BrowserRouter>
+        <Header />
+        <div className="w-full h-screen flex flex-col">
+          <AppRoutes />
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
--- src/components/Header.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-function Header() {
-    return ( 
-        <h1>Task Manager</h1>
-     );
-}
-
-export default Header;
