606614679ccf81adc3cc3a8c75f074acfc97fc9b
Abhik Salian <21e03.abhik@sjec.ac.in>
2024-08-24 13:59:32 +0530

    removed EditTask.js file...included edit functionality in Task.js itself

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useAuth } from '../../contexts/authContext';
 import Task from './Task';
-import EditTask from './EditTask';
 const Home = () => {
     const { currentUser } = useAuth()
     return (
