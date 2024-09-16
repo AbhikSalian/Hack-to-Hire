1c30ef505f7165cc15459f9b6fe366c0e5c2d5a3
Abhik Salian <21e03.abhik@sjec.ac.in>
2024-09-16 07:25:15 +0530

    sample8

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,45 +3,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 require('dotenv').config();
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Firebase configuration for your web app
-// Replace these with your Firebase project's configuration details
-// Note: Keep the API keys and other sensitive information secure
-
-// Uncomment and use the appropriate configuration as needed
-
-// Firebase configuration for the "hack-to-hire" project
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC1DVW7IGdhaLe_mmpgPuFb2NZHRSWzGAs",
-//   authDomain: "hack-to-hire.firebaseapp.com",
-//   projectId: "hack-to-hire",
-//   storageBucket: "hack-to-hire.appspot.com",
-//   messagingSenderId: "166434181364",
-//   appId: "1:166434181364:web:bf4a816a1da1cac3244ea1",
-//   measurementId: "G-LWZHJP2MRJ"
-// };
-
-// Firebase configuration for the "task-manager-ce02f" project
-// const firebaseConfig = {
-//   apiKey: "AIzaSyA6sfdICEf8fbEoDh0I9Zu780zRBniYuNw",
-//   authDomain: "task-manager-ce02f.firebaseapp.com",
-//   projectId: "task-manager-ce02f",
-//   storageBucket: "task-manager-ce02f.appspot.com",
-//   messagingSenderId: "26997091343",
-//   appId: "1:26997091343:web:c4c4ffa39ecfbae720077a"
-// };
-
-// Firebase configuration for the "task-manager-app-e2dab" project
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC47t9re6uhwc_bGqaqFBzdUeM2mk-R360",
-//   authDomain: "task-manager-app-e2dab.firebaseapp.com",
-//   projectId: "task-manager-app-e2dab",
-//   storageBucket: "task-manager-app-e2dab.appspot.com",
-//   messagingSenderId: "334558603716",
-//   appId: "1:334558603716:web:9feaa911a8844c43fb9c91"
-// };
 
 // Firebase configuration for the "task-manager-application-b016b" project
 const firebaseConfig = {
